8df14bf9fe35ed58de6d5dd25dc916a4543cb1dd
220031-Miao Heng <1098011272@qq.com>
2023-12-19 14:19:06 +0000

    实现简单itoa函数和sprintf函数

--- tests/cpu-tests/tests/string.c
+++ tests/cpu-tests/tests/string.c
@@ -14,6 +14,12 @@ char str[20];
 
 int main() {
 	check(strcmp(s[0], s[2]) == 0);
+	check(strncmp(s[0], s[2], 10) == 0);
+	check(strncmp(s[3], s[4], 10) < 0);
+	check(strncmp(s[0], s[2], strlen(s[0])) == 0);
+	check(strncmp(s[0], s[1], strlen(s[0])) < 0);
+	check(strncmp(s[5], "######", 6) < 0);
+	
 	check(strcmp(s[0], s[1]) < 0);
 	check(strcmp(s[0] + 1, s[1] + 1) < 0);
 	check(strcmp(s[0] + 2, s[1] + 2) < 0);
@@ -26,5 +32,7 @@ int main() {
 
 	check(strlen(s[5]) == 5);
 
+	check(memcmp(strncpy(str, s[5], 4), "####", 4) == 0);
+	check(memcmp(strncpy(str, s[5], 6), "#####", 5) == 0);
 	return 0;
 }
