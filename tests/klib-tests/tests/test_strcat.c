b34f4be2e15aec33d5d31e0c6021a9b18c882b2c
220031-Miao Heng <1098011272@qq.com>
2023-12-28 11:24:20 +0000

    strcat通过测试,存在区域重叠UB

--- tests/klib-tests/tests/test_strcat.c
+++ tests/klib-tests/tests/test_strcat.c
@@ -1,57 +1,69 @@
 #include <klibtest.h>
 
-// #define N 32
-// uint8_t data[N];
+#define N 32
+char data[2*N], data1[2*N];
+char* p = data;
 
-// void reset() {
-//   int i;
-//   for (i = 0; i < N; i ++) {
-//     data[i] = i + 1;
-//   }
-// }
-
-// // 检查[l,r)区间中的值是否依次为val, val + 1, val + 2...
-// void check_seq(int l, int r, int val) {
-//   int i;
-//   for (i = l; i < r; i ++) {
-//     assert(data[i] == val + i - l);
-//   }
-// }
-
-// // 检查[l,r)区间中的值是否均为val
-// void check_eq(int l, int r, int val) {
-//   int i;
-//   for (i = l; i < r; i ++) {
-//     assert(data[i] == val);
-//   }
-// }
-
-// void test_memset() {
-//   int l, r;
-//   for (l = 0; l < N; l ++) {
-//     for (r = l + 1; r <= N; r ++) {
-//       reset();
-//       uint8_t val = (l + r) / 2;
-//       memset(data + l, val, r - l);
-//       check_seq(0, l, 1);
-//       check_eq(l, r, val);
-//       check_seq(r, N, r + 1);
-//     }
-//   }
-// }
-
-int main(const char *args) {
-  const char *fmt =
-    "Hello, AbstractMachine!\n"
-    "mainargs = '%'.\n"
-    "mem test!\n";
-
-  for (const char *p = fmt; *p; p++) {
-    // (*p == '%') ? putstr(args) : putch(*p);
-    putch(*p);
+void reset() {
+  int i;
+  for (i = 0; i < 2 * N; i ++) {
+    if(i < N) {
+      data[i] = i + 1;
+      data1[i] = i + 1;
+    } else {
+      data[i] = 0;
+      data1[i] = 0;
+    }
   }
 
-    test_memset();
+}
+
+void check_seq(int l, int r, int val) {
+  int i;
+  for (i = l; i < r; i ++) {
+    assert(data[i] == val + i - l);
+  }
+}
+
+void check_eq(int l, int r, int val) {
+  int i;
+  for (i = l; i < r; i ++) {
+    assert(data[i] == val);
+  }
+}
+
+void printf_data() {
+  for (int i = 0; i < 2*N; i ++) {
+    printf("%d\t", data[i]);
+  }
+  printf("\n");
+}
+
+void test_strcat() {
+  int l;
+  for (l = 0; l < N; l ++) {
+      reset();
+      // printf("%d\n", l);
+      // printf_data();
+      strcat(data, data1 + l);
+      // printf_data();
+      check_seq(0, N, 1);
+      check_seq(N, 2*N - l, l + 1);
+      check_eq(2*N - l, 2 *N, 0);    
+  }
+}
+
+int main(const char *args) {
+  
+  test_strcat();
+ 
+      // reset();
+      // printf_data();
+      // strcat(data, data + 25);
+      // printf_data();
+      // check_seq(0, N, 1);
+      // check_seq(N, 2*N - 25, 26);
+      // check_eq(2*N - 25, 2*N, 0);
 
   return 0;
 }
